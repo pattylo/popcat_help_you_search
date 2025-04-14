6fef887eaacc2a920af59edf32a996121b4a2312
pattylo <19451945as@gmail.com>
2025-04-14 20:00:56 +0800

    test

---
+++ ubuntu/app.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Create directory
+sudo rm -r ~/.local/share/popcat
+mkdir -p ~/.local/share/popcat
+
+# Copy files
+cp popcat ~/.local/share/popcat/
+cp popcat.png ~/.local/share/popcat/
+
+# Create .desktop file
+cat << EOF > ~/.local/share/applications/popcat.desktop
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=popcat
+Comment=Make popcat pop!
+Exec=$HOME/.local/share/popcat/popcat
+Icon=$HOME/.local/share/popcat/popcat.png
+Terminal=false
+Categories=Utility;
+EOF
+
+# Make it executable
+chmod +x ~/.local/share/applications/popcat.desktop
+
+update-desktop-database ~/.local/share/applications
+
+echo "Installed! Search 'Popcat' in your app launcher."
